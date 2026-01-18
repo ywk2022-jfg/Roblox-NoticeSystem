a5ca22b566f49487e5631ff59a83365a47e15026
ywk2022-jfg <ywk@jfgblog.top>
2026-01-18 13:54:45 +0800

    Create TriggerScript01.lua

---
+++ TriggerScript01.lua
@@ -0,0 +1,61 @@
+--This file is a Localscript in Roblox Studio
+	
+	local ReplicatedStorage = game:GetService('ReplicatedStorage')
+	local RemoveEvent = ReplicatedStorage.NoticeSystem.Creatnotice
+	RemoveEvent.OnClientEvent:Connect(function(Text,Title,sec: number)
+		local s = false
+		local Frame = script.notice:Clone()
+		Frame.Parent = script.Parent.Parent.Main
+		Frame:TweenSize(UDim2.new(1, 0,0.186, 0),
+			Enum.EasingDirection.Out,
+			Enum.EasingStyle.Sine,
+			0.5,
+			false
+		)
+		Frame.Title.Text = Title
+		Frame.Maintext.Text = Text
+		local Frame2 = script.notice2:Clone()
+		Frame2.Parent = game.Workspace.notiepad.SurfaceGui.Main.ScrollingFrame
+		Frame2.Title.Text = Title
+		Frame2.Maintext.Text = Text
+		local Button = Frame.TextButton
+		Button.Interactable = true
+		Button.MouseEnter:Connect(function()
+			Button.BackgroundTransparency = 0.45
+			Button.TextTransparency = 0
+			Button.MouseLeave:Connect(function()
+				Button.BackgroundTransparency = 1
+				Button.TextTransparency = 1
+			end)
+			Button.MouseButton1Click:Connect(function()
+			Frame:TweenSize(UDim2.new(0, 0,0.186, 0),
+				Enum.EasingDirection.Out,
+				Enum.EasingStyle.Sine,
+				0.5,
+				false
+			)
+			wait(0.5)
+				Frame:Destroy()
+				s = true
+			end)
+		end)
+	local a = 0
+		while wait(1) do
+			if a == sec then
+			Frame:TweenSize(UDim2.new(0, 0,0.186, 0),
+				Enum.EasingDirection.Out,
+				Enum.EasingStyle.Sine,
+				0.5,
+				false
+			)
+			wait(0.5)
+				Frame:Destroy()
+				s = false
+				break
+			elseif a ~= sec and s ~= true then
+				a = a + 1
+				print(a,sec)
+			end
+		end
+	end)
+--end)
